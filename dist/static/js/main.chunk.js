0c165981fb69d0afce43949dda298208a95fb192
王金海(haha.wang) <wanghaha@huolala.cn>
2021-06-30 17:35:07 +0800

    修改webpack配置

---
+++ dist/static/js/main.chunk.js
@@ -0,0 +1,15 @@
+(this["webpackJsonpmy-app"] = this["webpackJsonpmy-app"] || []).push([["main"],{
+
+/***/ "./src/index.tsx":
+/*!***********************!*\
+  !*** ./src/index.tsx ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: [BABEL] /Users/wanghaha/code/wangdashuaihenshuai.github.io/src/index.tsx: React Refresh Babel transform should only be enabled in development environment. Instead, the environment is: \"production\". If you want to override this check, pass {skipEnvCheck: true} as plugin options. (While processing: \"/Users/wanghaha/code/wangdashuaihenshuai.github.io/node_modules/react-refresh/babel.js\")\n    at module.exports (/Users/wanghaha/code/wangdashuaihenshuai.github.io/node_modules/react-refresh/cjs/react-refresh-babel.production.min.js:15:488)\n    at /Users/wanghaha/code/wangdashuaihenshuai.github.io/node_modules/@babel/core/lib/config/full.js:211:14\n    at Generator.next (<anonymous>)\n    at Function.<anonymous> (/Users/wanghaha/code/wangdashuaihenshuai.github.io/node_modules/@babel/core/lib/gensync-utils/async.js:26:3)\n    at Generator.next (<anonymous>)\n    at step (/Users/wanghaha/code/wangdashuaihenshuai.github.io/node_modules/gensync/index.js:261:32)\n    at evaluateAsync (/Users/wanghaha/code/wangdashuaihenshuai.github.io/node_modules/gensync/index.js:291:5)\n    at Function.errback (/Users/wanghaha/code/wangdashuaihenshuai.github.io/node_modules/gensync/index.js:113:7)\n    at errback (/Users/wanghaha/code/wangdashuaihenshuai.github.io/node_modules/@babel/core/lib/gensync-utils/async.js:70:18)\n    at async (/Users/wanghaha/code/wangdashuaihenshuai.github.io/node_modules/gensync/index.js:188:31)");
+
+/***/ })
+
+},[["./src/index.tsx","runtime-main"]]]);
+//# sourceMappingURL=main.chunk.js.map
